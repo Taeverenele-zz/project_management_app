5289bd92997e453893dfa75b1e9695fa25297827
Nele Taevere <ntaevere@gmail.com>
2020-10-13 12:41:52 +1000

    add view pages

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,43 +1,59 @@
 class ProjectsController < ApplicationController
-  before_action :read_projects
-  skip_before_action :verify_authenticity_token
   
   def index
-    # send back all of the resources to the client
-    render json: @projects
+    #show all of the resources
   end
-
+  def new
+    #to have a form that allows you to create a new resource
+  end
   def show
-    # show is for sending back only one resource
-    found_project = @projects.find do |project|
-      project["id"] == params[:id].to_i
-    end
-
-    render json: found_project
+    #show one of the resources
   end
-
-  def create
-    new_project = {
-      id: params[:id].to_i, 
-      name: params[:name], 
-      github_status: is_true?(params[:github_status])}
-    @projects << new_project
-    write_projects(@projects)
-    redirect_to projects_path
-  end
-
-  private
-
-  def is_true?(bool)
-    bool == true
-  end
-
-  def write_projects(projects)
-    File.write(Rails.public_path.join("projects.json"), JSON.generate(projects))
-  end
-
-  def read_projects
-    @projects = JSON.parse(File.read(Rails.public_path.join("projects.json")))
-  end
-
+  # def create
+  # end
 end
+
+
+# class ProjectsController < ApplicationController
+#   before_action :read_projects
+#   skip_before_action :verify_authenticity_token
+  
+#   def index
+#     # send back all of the resources to the client
+#     render json: @projects
+#   end
+
+#   def show
+#     # show is for sending back only one resource
+#     found_project = @projects.find do |project|
+#       project["id"] == params[:id].to_i
+#     end
+
+#     render json: found_project
+#   end
+
+#   def create
+#     new_project = {
+#       id: params[:id].to_i, 
+#       name: params[:name], 
+#       github_status: is_true?(params[:github_status])}
+#     @projects << new_project
+#     write_projects(@projects)
+#     redirect_to projects_path
+#   end
+
+#   private
+
+#   def is_true?(bool)
+#     bool == true
+#   end
+
+#   def write_projects(projects)
+#     File.write(Rails.public_path.join("projects.json"), JSON.generate(projects))
+#   end
+
+#   def read_projects
+#     @projects = JSON.parse(File.read(Rails.public_path.join("projects.json")))
+#   end
+
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/projects', to: 'projects#index'
+  get '/projects/new', to: 'projects#new'
   get '/projects/:id', to: 'projects#show'
-  post '/projects', to: 'projects#create'
+  # get '/projects/:id', to: 'projects#show'
+  # post '/projects', to: 'projects#create'
 end
